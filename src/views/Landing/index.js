a98c3c7a53ac3070e1e11a88bc099b1ca7720fee
patorseing <patorseing@gmail.com>
2019-05-08 16:53:19 +0700

    fix by cherry

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -18,7 +18,7 @@ class Landing extends Component {
             <Fragment>
                 {isMobile ?
                     <svg
-                        width="100%" height="400"
+                        width="100%" height="480"
                         xmlns="http://www.w3.org/2000/svg"
                         xmlnsXlink="http://www.w3.org/1999/xlink"
                     >
